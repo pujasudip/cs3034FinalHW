e30f1728e22891a1b2240103c136e6b1569221f9
Sudip Baral <pujasudip@gmail.com>
2016-12-08 16:45:30 -0800

    revise

--- final.cpp
+++ final.cpp
@@ -9,8 +9,10 @@
 using namespace std;
 
 void replace(node *root, node *runner);
-void print(node *root, node *runner);
+void print(node *root);
 void deleteNode(node *root, node *runner);
+int sizeOfList(node *root, node *runner);
+void split(node *root, node *runner, int size);
 
 int main(){
 	node *root;
@@ -34,15 +36,18 @@ int main(){
 		}
 		cout << endl;
 	}
-	print(root, runner);
+	print(root);
 	replace(root, runner);
 	deleteNode(root, runner);
-	print(root, runner);
+	print(root);
+	int size = sizeOfList(root, runner);
+	split(root, runner, size);
 }
 
-void print(node *root, node *runner){
+void print(node *printData){
+	node *runner = new node;
 	cout << "List: ";
-	runner = root;
+	runner = printData;
 	if(runner != 0){
 		while(runner != 0){
 			cout << runner->data << " ";
@@ -68,28 +73,86 @@ void replace(node *root, node *runner){
 			runner = runner->next;
 		}
 	}
-	print(root, runner);
 }
 
 void deleteNode(node *root, node *runner){
-	while(root->data > 50){
-		node *tmp = root->next;
-		delete root;
-		root = tmp;
+	runner = root;
+	if(runner != NULL){
+		node *temp = runner;
+		while(temp->next != NULL){
+			if(temp->next->data <= 50){
+				runner->next = temp->next;
+				runner = runner->next;
+			}
+			temp = temp->next;
+		}
 	}
+	//deleting tail
+	if(runner->next->data > 50){
+		runner->next = NULL;
+	}
+}
+int sizeOfList(node *root, node *runner){
+	int size = 0;
 	runner = root;
 
-	node *nn = root->next;
-
-	while(runner != NULL){
-		while(nn->data > 50){
-			nn = nn->next;
-		}
-		runner = nn->next;
+	while(runner->next != NULL){
+		size++;
+		runner = runner->next;
 	}
+	return size;
+}
+void split(node *root, node *runner, int size){
+	int firstHalf = size/2;
+	cout << "half: " << firstHalf << endl;
+	int secondHalf = size - firstHalf;
+	cout << "half: " << secondHalf << endl;
+	int count = 1;
 
+	node *list1Head;
+	node *list1;
+	list1 = list1Head;
 
+	runner = root;
 
+	while(count < firstHalf){
+		node *temp = new node;
+		temp->data = runner->next->data;
+		list1->next =  temp;
+		list1 = list1->next;
+		runner = runner->next;
+		cout << "hello:" << list1->data << endl;
+		count++;
+		
+	}
+	print(list1Head);
+
+	node *list2Head;
+	node *list2;
+	list2 = list2Head;
+
+	count = 0;
+	while(count < secondHalf){
+		node *temp = new node;
+		temp->data = runner->next->data;
+		list2->next =  temp;
+		list2 = list2->next;
+		runner = runner->next;
+		cout << "list2:" << list2->data << endl;
+		count++;
+	}
+	print(list2Head);
 }
 
 #endif
+
+
+
+
+
+
+
+
+
+
+
